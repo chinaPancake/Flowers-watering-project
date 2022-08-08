d295d62ee850fc203b7ade40cb83454b18dcf824
chinaPancake <sitletto@gmail.com>
2022-08-08 23:10:03 +0200

    fix bug with date type, and code cleaning

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import calendar
 import sqlite3
 import datetime
 
@@ -28,6 +27,7 @@ def select_all_task(conn):
 conn = sqlite3.connect('flowers.db')
 cur = conn.execute('SELECT * FROM flowers')
 records = cur.fetchall()
+
 #get all info about flower
 for row in records:
      print("Name: ", row[0])
@@ -36,16 +36,14 @@ for row in records:
      print("How_Often: ", row[3])
      print("\n")
 
-#All inputs of the flower, prolly later im gonna do it in list of lists so we can get acess to every flower insted of just one
-flowers = []
 #flower = Flower(name=input('What is the name of the flower: '), once_every=int(input('How many days till another watering: ')), how_much=float(input('How much water in ml: ')))
 flower = Flower(name=cur.execute('SELECT name FROM flowers'), once_every=cur.execute('SELECT once_every FROM flowers'), how_much=cur.execute('SELECT how_much FROM flowers'))
 
-flowers.append(flower.name)
-flowers.append(flower.once_every)
-flowers.append(flower.how_much)
+#Dates
+now = datetime.datetime.now().date()
+base = datetime.datetime.today().date()
+next_watering = base + datetime.timedelta(days=row[2])
+last_watering = base - datetime.timedelta(days=row[2])
 
-now = datetime.datetime.now()
-base = datetime.datetime.today()
-print('Today is: ',now.strftime("%A"), ' next watering should be done in: ', base+datetime.timedelta(days=row[2]), f' and you should add {row[3]} ml of water')
-print('Last watering was', base-datetime.timedelta(days=row[2]))
+print('Today is: ',now.strftime("%A"), f'({now})', ' next watering should be done in: ', next_watering.strftime("%A"), f'({next_watering})' , f' and you should add {row[3]} ml of water')
+print('Last watering was', last_watering.strftime("%A") ,f'({last_watering})')
