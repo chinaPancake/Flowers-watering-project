663bd6ea2e877210d2fa5cfe331470cb52cdd313
chinaPancake <sitletto@gmail.com>
2022-08-09 23:35:37 +0200

    New variable name, and .gitignore file

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import sqlite3
 import datetime
 
 class Flower():
-    def __init__(self, name, description, once_every, how_much, last_watering):
+    def __init__(self, name, description, frequency, amount, last_watering):
         self.name = name
         self.description = description
-        self.once_every = once_every
-        self.how_much = how_much
+        self.frequency = frequency
+        self.amount = amount
         self.last_watering = last_watering
 
 f_name = input('Flower name: ')
 f_description = input('Flower description: ')
-f_once_every = input('Once every day in a week, this plant need watering: ')
-f_how_much = input('How much water in ml does this flower need: ')
+f_frequency = input('Frequency of watering: ')
+f_amount = input('Amount of water to watering in ml: ')
 f_last_watering = input('When was the last watering?(format of input yyyy-mm-dd): ')
 
 # connect with database
@@ -21,7 +21,7 @@ conn = sqlite3.connect('flowers.db')
 # insert new flower into database
 sql = f""" INSERT INTO flowers
             (name, description, once_every, how_much, last_watering)
-            VALUES('{f_name}', '{f_description}', '{f_once_every}', '{f_how_much}', '{f_last_watering}')"""
+            VALUES('{f_name}', '{f_description}', '{f_frequency}', '{f_amount}', '{f_last_watering}')"""
 
 cur = conn.cursor()
 cur = conn.execute('SELECT * FROM flowers')
@@ -40,7 +40,7 @@ for row in records:
      print("\n")
 
 #get data to class, and create object flower from class Flower
-flower = Flower(name=row[0],description=row[1], once_every=row[2], how_much=row[3], last_watering=row[4])
+flower = Flower(name=row[0], description=row[1], frequency=row[2], amount=row[3], last_watering=row[4])
 
 #Dates
 now = datetime.datetime.now().date()
@@ -48,5 +48,5 @@ base = datetime.datetime.today().date()
 next_watering = base + datetime.timedelta(days=row[2])
 last_watering = base - datetime.timedelta(days=row[2])
 
-print('Today is: ',now.strftime("%A"), f'({now})', ' next watering should be done in: ', next_watering.strftime("%A"), f'({next_watering})' , f' and you should add {row[3]} ml of water')
-print('Last watering was', last_watering.strftime("%A") ,f'({last_watering})')
+'''print('Today is: ',now.strftime("%A"), f'({now})', ' next watering should be done in: ', next_watering.strftime("%A"), f'({next_watering})' , f' and you should add {row[3]} ml of water')
+print('Last watering was', last_watering.strftime("%A") ,f'({last_watering})')'''
