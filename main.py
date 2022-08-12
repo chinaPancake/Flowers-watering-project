f6759283239d6d1f2ef716b5356721c006f7fcfc
chinaPancake <sitletto@gmail.com>
2022-08-12 16:48:40 +0200

    Rewrite all to functions to keep while loop simple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sqlite3
 from datetime import datetime, date, timedelta
-from sqlite3 import Cursor
-
 
 class Flower:
     def __init__(self, name, description, frequency, amount, last_watering):
@@ -15,6 +13,75 @@ def print_menu():
     for key in menu_options.keys():
         print(menu_options[key])
 
+connect = sqlite3.connect('flowers.db')
+c = connect.cursor()
+c.execute('''SELECT * FROM flowers''')
+records = c.fetchall()
+connect.commit()
+
+def new_flower():
+    f_name = input('Flower name: ')
+    f_description = input('Flower description: ')
+    f_frequency = input('Frequency of watering: ')
+    f_amount = input('Amount of water to watering in ml: ')
+    f_last_watering = input('When was the last watering?(format of input yyyy-mm-dd): ')
+    sql = f""" INSERT INTO flowers
+                        (name, description, frequency, amount, last_watering)
+                        VALUES('{f_name}', '{f_description}', '{f_frequency}', '{f_amount}', '{f_last_watering}')"""
+    c.execute(sql)
+    connect.commit()
+    print(f'You add {f_name} to database!')
+
+def delete_flower():
+    print('-------------')
+    for name in records:
+        print('flower name: ', name[0])
+    f_delete = input('What flower do you want to delete? (put flower name, if u want to go back put quit): ')
+    if f_delete.lower() == 'quit':
+        print('-------------')
+        pass
+    elif f_delete.lower() != 'quit':
+        c.execute(f"""DELETE FROM flowers WHERE name LIKE '{f_delete}'""")
+        print(f'You just delete {f_delete}')
+        connect.commit()
+        print('-------------')
+
+def show_flowers():
+    print('-------------')
+    for row in records:
+        print("Name: ", row[0])
+        print("Description: ", row[1])
+        print("once_every: ", row[2])
+        print("How_much in ml: ", row[3])
+        print('Last watering: ', row[4])
+        print('-------------')
+
+def next_watering():
+    from datetime import date
+    print('-------------')
+    print('Today is: ', date.today())
+    print('-------------')
+    for date in records:
+        dateint = datetime.strptime(date[4], '%Y-%m-%d')
+        timedelta_dateint = dateint + timedelta(days=date[2])
+        print('Last watering of ', date[0], f'(water every {date[2]} days)', ' was', date[4],
+              'Next watering should be done in ', timedelta_dateint.strftime("%Y-%m-%d"))
+    print('-------------')
+
+def save_to_file():
+    print('Save to file')
+    with open('share.txt', 'w', encoding='utf-8') as save_to_file:
+        for save in records:
+            dateint = datetime.strptime(save[4], '%Y-%m-%d')
+            timedelta_dateint = dateint + timedelta(days=save[2])
+            save_to_file.write(f'Flower name: {save[0]} \n'),
+            save_to_file.write(f'Flower Description:  {save[1]} \n'),
+            save_to_file.write(f'Flower Once_every:  {save[2]} \n'),
+            save_to_file.write(f'Flower How Much water:  {save[3]} \n'),
+            save_to_file.write(f'Flower Last Watering:  {save[4]} \n'),
+            save_to_file.write(f'Flower Next Watering: {timedelta_dateint.strftime("%Y-%m-%d")}\n')
+            save_to_file.write('\n ------------------ \n\n'),
+
 menu_options= {
     1: '1. Add new flower',
     2: '2. Delete flower from database',
@@ -23,84 +90,21 @@ menu_options= {
     5: '5. Save to file',
     6: '6. Exit',
 }
-# connect with database
-conn = sqlite3.connect('flowers.db')
-cur = conn.cursor()
-cur = conn.execute('SELECT * FROM flowers')
-records = cur.fetchall()
-conn.commit()
 
 while(True):
     print_menu()
-    option = int(input('Select from menu: '))
-    # get all inputs
+    option = int(input('Chose menu option: '))
     if option == 1:
-        f_name = input('Flower name: ')
-        f_description = input('Flower description: ')
-        f_frequency = input('Frequency of watering: ')
-        f_amount = input('Amount of water to watering in ml: ')
-        f_last_watering = input('When was the last watering?(format of input yyyy-mm-dd): ')
-        # insert new flower into database
-        sql = f""" INSERT INTO flowers
-                    (name, description, frequency, amount, last_watering)
-                    VALUES('{f_name}', '{f_description}', '{f_frequency}', '{f_amount}', '{f_last_watering}')"""
-        sql_execute = conn.execute(sql)
-        conn.commit()
-        print('Insert operation ok! ')
-
-    # Delete from database
+        new_flower()
     elif option == 2:
-        print('-------------')
-        for name in records:
-            print('flower name: ', name[0])
-        f_delete = input('What flower do you want to delete? (put flower name, if u want to go back put quit): ')
-        if f_delete.lower() == 'quit':
-            print('-------------')
-            pass
-        elif f_delete.lower() != 'quit':
-            cur.execute(f"""DELETE FROM flowers WHERE name LIKE '{f_delete}'""")
-            print(f'You just delete {f_delete}')
-            conn.commit()
-            print('-------------')
-
-    # get all info about flower
+        delete_flower()
     elif option == 3:
-        print('-------------')
-        for row in records:
-            print("Name: ", row[0])
-            print("Description: ", row[1])
-            print("once_every: ", row[2])
-            print("How_much in ml: ", row[3])
-            print('Last watering: ', row[4])
-            print('-------------')
-    # When gonna be next watering
+        show_flowers()
     elif option == 4:
-        print('-------------')
-        print('Today is: ', date.today())
-        print('-------------')
-        for date in records:
-            dateint = datetime.strptime(date[4], '%Y-%m-%d')
-            timedelta_dateint = dateint + timedelta(days=date[2])
-            print('Last watering of ', date[0], ' was', date[4],
-                  'and this flower you have to water every: ', date[2], 'days.', '\n',
-                  'Next watering should be done in ', timedelta_dateint.strftime("%Y-%m-%d"))
-        print('-------------')
-    # SAVE TO FILE
+        next_watering()
     elif option == 5:
-        print('Save to file')
-        with open('share.txt', 'w', encoding='utf-8') as save_to_file:
-            for save in records:
-                dateint = datetime.strptime(save[4], '%Y-%m-%d')
-                timedelta_dateint = dateint + timedelta(days=save[2])
-                save_to_file.write(f'Flower name: {save[0]} \n'),
-                save_to_file.write(f'Flower Description:  {save[1]} \n'),
-                save_to_file.write(f'Flower Once_every:  {save[2]} \n'),
-                save_to_file.write(f'Flower How Much water:  {save[3]} \n'),
-                save_to_file.write(f'Flower Last Watering:  {save[4]} \n'),
-                save_to_file.write(f'Flower Next Watering: {timedelta_dateint.strftime("%Y-%m-%d")}\n')
-                save_to_file.write('\n ------------------ \n\n'),
-    # EXIT
+        save_to_file()
     elif option == 6:
         print("Thanks for today! Let's grow togheter! ")
-        conn.commit()
+        connect.commit()
         exit()
